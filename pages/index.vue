adfeb3390fb6eefffb10eb35e5fd4e5f8cfc36ce
carpstrim <k.yasumi.jug@gmail.com>
2019-08-11 18:38:13 +0900

    chanded package.json

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
 <div>
+
+        <v-btn fab large fixed right bottom dark 
+        color="warning" 
+        style="bottom: 80px">
+        <v-icon dark>add</v-icon>
+        </v-btn>
+        
   <h2>Hello World</h2>
+<v-container>
     <v-layout>
   <v-icon>add</v-icon>
   </v-layout>
+</v-container>
 </div>
 </template>
 
