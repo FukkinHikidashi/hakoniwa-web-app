91b6106fbfa89a2bf5c0503b652548ed76dc46c7
carpstrim <k.yasumi.jug@gmail.com>
2019-08-13 18:51:32 +0900

    started creating main mission page

--- pages/mainMission.vue
+++ pages/mainMission.vue
@@ -1,9 +1,26 @@
 <template>
 <div>
   <h2>This is mainMission</h2>
+  <v-container>
+    <v-card color="warning" class="ma-2 align-center justify-center" style="height: 30vh"
+    @click="dialog = true">
+      <p>ここにミッションの内容を表示</p>
     <v-layout>
-  <v-icon>add</v-icon>
   </v-layout>
+    </v-card>
+  </v-container>
+
+  <v-dialog v-model="dialog">
+    <v-card>
+    ここに回答ボタンを設置
+    </v-card>
+    </v-dialog>
+
+  <!--ビンゴカード -->
+
+
+
+  
 </div>
 </template>
 
@@ -11,6 +28,19 @@
   export default{
     data() {
       return{
+        dialog: false,
+        nowStage: 1,
+        missionDatas: [
+          {
+            id: 1,
+            title: "mission1",
+            text: "ここに問題文",
+            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
+            answerType: "text", // ["text", "photo"]
+            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
+            difficulty: 1 // num
+          }
+        ]
 
       }
     }
