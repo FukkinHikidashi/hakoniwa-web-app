79f0400c2dab0a47454350481ded40de876634dc
FukkinHikidashi <s.t.loveoudc09@gmail.com>
2019-08-14 23:39:34 +0000

    subMission layout 80% done

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,29 @@
 
 
   <h2>Hello World</h2>
-<v-container>
-    <v-layout>
-  <v-img
-  src="https://www.homepage-tukurikata.com/image/lion.jpg"
-  cover
-  />
-  </v-layout>
-</v-container>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn text>Button</v-btn>
+      <v-btn text>Button</v-btn>
+    </v-card-actions>
+  </v-card>
 </div>
 </template>
 
--- pages/subMission.vue
+++ pages/subMission.vue
@@ -1,20 +1,94 @@
 <template>
-<div>
-  <h2>This is subMission</h2>
-    <v-layout>
-  <v-icon>add</v-icon>
-  </v-layout>
-</div>
+  <div style="margin-top:10px;width:96vw; margin-left:auto;margin-right:auto;">
+    <div >
+      <div style="background-color:rgb(91,192,222,0.5);width:30%;text-align:center">
+        <h3>ストーリー</h3>
+      </div>
+      <div style="margin-top:5px;margin-bottom:10px;border:solid 2px rgb(91,192,222,0.5)">
+        むかしむかしあるところに
+
+      </div>
+    </div>
+
+    <div style="background-color:rgb(91,192,222,0.5);width:30%;text-align:center">
+        <h3>ミッション</h3>
+    </div>
+
+    <v-expansion-panels
+      v-model="panel"
+      :disabled="disabled"
+      multiple
+      style="margin-top:5px"
+    >
+      <v-expansion-panel v-show="false" style="border:solid 2px rgb(91,192,222,0.5)">
+        <div style="background-color:rgb(91,192,222,0.5)">1.捕らわれの女の子</div>
+        <v-expansion-panel-header>
+          <div style="width:30%"><p>未回答</p></div>
+          <div style="width:70%"><p>キーワード：魚</p></div>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <div>
+            ストーリー
+          </div>
+          <div>
+            問題文
+          </div>
+
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <div>2.#########</div>
+        <v-expansion-panel-header>Panel 2</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Some content
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <div>3.#########</div>
+        <v-expansion-panel-header>Panel 3</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Some content
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <div>4.#########</div>
+        <v-expansion-panel-header>Panel 4</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Some content
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+      <v-expansion-panel>
+        <div>5.#########</div>
+        <v-expansion-panel-header>Panel 5</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          Some content
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script>
-  export default{
-    data() {
-      return {
-
-      }
-    }
-    }
-  
-
+  export default {
+    data: () => ({
+      panel: [0, 1, 2, 3],
+//      mission:{
+//       quiz1:{
+//          title: "1.捕らわれの女の子",
+//          text:,
+//          story:,
+//          reply:,
+//          photoUrl:,
+//          keyword:,
+//          answers:,
+//        },
+//      },
+    }),
+  }
 </script>
