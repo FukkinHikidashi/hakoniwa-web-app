263635ca71b83b6f44c86baf9d78a6e480519d43
carpstrim <k.yasumi.jug@gmail.com>
2019-08-16 14:32:25 +0900

    made test.vue in pages for firebase register

--- pages/mainMission.vue
+++ pages/mainMission.vue
@@ -79,7 +79,7 @@ v-model="tab">
 </v-tab-item>
 
 <v-tab-item>
-  <v-card color="light-blue lighten-4" flat>
+<!--  <v-card color="light-blue lighten-4" flat>
   <v-container v-if="nowStage >= 3">
       <v-layout justify-center wrap width="100vw">
       <v-card
@@ -108,7 +108,7 @@ v-model="tab">
   <v-container v-else>
     まだだよ
   </v-container>
-      </v-card>
+      </v-card>-->
 
 </v-tab-item>
 
@@ -158,6 +158,19 @@ v-model="tab">
 
 <script>
   export default{
+    beforeCreate(){
+      this.$firestore.collection("Mission").get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.missionDatas.push(doc.data())
+      console.log(this.missionDatas)
+
+      
+
+        })
+      })
+      console.log("aaa")
+    },
     data() {
       return{
         dialog: false,
@@ -165,140 +178,12 @@ v-model="tab">
         tab: null,
         displayMission: 1,
         nowDisplayText: "aaa",
-        missionDatas: [
-          {
-            id: 1,
-            title: "mission1",
-            text: "犬の写真をとる！",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1, // num
-            status: "grey"
-          },{
-            id: 2,
-            title: "mission2",
-            text: "千成ひょうたんの写真をとる！",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1, // num
-            status: "yellow"
-          },{
-            id: 3,
-            title: "mission3",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1, // num
-            status: ""
-          },{
-            id: 4,
-            title: "mission4",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 5,
-            title: "mission5",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 6,
-            title: "mission6",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 7,
-            title: "mission7",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 8,
-            title: "mission8",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 9,
-            title: "mission9",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 10,
-            title: "mission10",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 11,
-            title: "mission11",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 12,
-            title: "mission12",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 13,
-            title: "mission13",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 14,
-            title: "mission14",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 15,
-            title: "mission15",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          },{
-            id: 16,
-            title: "mission16",
-            text: "ここに問題文",
-            photoUrl: "https://app-senbonknock.com/wp-content/uploads/2019/05/icon.png",
-            answerType: "text", // ["text", "photo"]
-            missionType: "eachPoint", // ["eachPoint", "everywhere", "quiz"]
-            difficulty: 1 // num
-          }
-        ]
+        missionDatas: [],
+        stage1Missions:[],
+        stage2Missions:[],
+        stage3Missions:[],
+        stage4Missions:[]
+
 
       }
     },
---
+++ pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- firebase登録用にとりあえず作ったやつをおいておく場所-->
+    <v-btn @click="createTeamData()">create</v-btn>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            teamData: {}
+
+        }
+    },
+    methods:{
+        createTeamData(){
+            this.$firestore.collection("Team").doc("teamXX").get()
+            .then(doc => {
+                this.$firestore.collection("Team").doc("wPenfOmOi4Y8ibxERyqzLU63TTe2").set(doc.data())
+            })
+        }
+
+    }
+}
+</script>
