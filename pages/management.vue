be9319de6ef93d1a86e975050095fa59cc7e0f7a
carpstrim <k.yasumi.jug@gmail.com>
2019-08-30 02:35:46 +0900

    mainMission approval system done

--- pages/management.vue
+++ pages/management.vue
@@ -1,5 +1,6 @@
 <template>
 <div>
+    <h3>承認画面</h3>
     <v-tabs
     v-model="tab"
     background-color="light-blue lighten-5"
@@ -14,24 +15,26 @@
 
     <v-tab-item>
     <div v-for="el in mainMissionAnswers" :key="el.key">
-        <v-container>
-            <v-card @click="nowCardDialog(el)">
+        <v-container class="background-color: grey lighten-1">
+            <v-card @click="nowCardDialog(el)" :color="el.color">
                 <v-container>
                 <v-layout>
                     <v-flex xs7>
-                        <h3>{{ el.team }}チーム</h3>
+                        <h3 style="text-decoration: underline">{{ el.team }}チーム</h3>
                         id:{{el.id}}<br>
                         mission:{{el.text}}<br>
 
 
                     </v-flex>
                     <v-flex xs5>
+                        <v-container>
                         <div v-if="el.answerType === 'text'">
                             回答：{{ el.answer }}
                         </div>
                         <div v-if="el.answerType === 'photo'">
                             <v-img :src="el.answer" aspect-ratio="1"></v-img>
                         </div>
+                        </v-container>
                     </v-flex>
                 </v-layout>
                 </v-container>
@@ -51,8 +54,14 @@
                     </v-container>
                 </v-layout>
             <v-layout justify-space-around>
-                <v-btn color="primary">承認</v-btn>
-                <v-btn color="secondary">却下</v-btn>
+                <v-btn color="primary"
+                @click="acceptMainMissionAnswer()"
+                :loading="loading"
+                :disabled="disabled">承認</v-btn>
+                <v-btn color="secondary"
+                @click="denyMainMissionAnswer()"
+                :disabled="disabled"
+                :loading="loading">却下</v-btn>
             </v-layout>
             </v-container>
         </v-dialog>
@@ -76,7 +85,7 @@ export default {
         .get()
         .then(snapshot => {
             snapshot.forEach(doc => {
-                this.mainMissionAnswers.push(doc.data())
+                this.mainMissionAnswers.push({...doc.data(),docId: doc.id,color:"grey lighten-3"})
             })
         })
 
@@ -89,11 +98,12 @@ export default {
     data(){
         return {
         dialog: false,
+        disabled: false,
         tab:null,
+        loading:false,
         mainMissionAnswers:[],
         nowCard:{},
         subStoryAnswers:[],
-
 
         }
     },
@@ -105,10 +115,96 @@ export default {
 
 
         },
-        acceptMissionAnswer(){
+        async acceptMainMissionAnswer(){
+            this.loading = await true
+            this.disabled = await true
+            const uid = await this.nowCard.uid
+            const docId = await this.nowCard.docId
+            const firestore = this.$firestore
+            const score = await this.nowCard.nowStage*10
+            const key = await this.nowCard.key
+            const id = await this.nowCard.id
+            //mainMissionAnswerのcollectionにまず情報更新
+            await firestore.doc(`mainMissionAnswer/${docId}`).update({clear: 5})
+            await console.log("done")
+
+            //点数の追加
+            await firestore.doc(`Team/${uid}`).get()
+            .then(doc => {
+                const nowScore = doc.data().point
+                nowScore.mainPoint = nowScore.mainPoint*1 + score
+                firestore.doc(`Team/${uid}`).update({point: nowScore})
+                console.log({nowScore})
+            })
+
+
+            //team側へのclearの更新
+            await firestore.doc(`Team/${uid}`).get()
+            .then(doc => {
+                const renewedData = doc.data().mainMission.map( x => {
+                    if(x.id === id){
+                        return {id:id, clear: 2}
+                    }else{
+                        return x
+                    }
+                    
+                })
+                firestore.doc(`Team/${uid}`).update({mainMission: renewedData})
+            })
+
+
+            this.nowCard.color = await "black"
+            await this.mainMissionAnswers.map(x => {
+                if(x.key === this.nowCard.key){ return this.nowCard }else{ return x }
+            })
+            this.loading = await false
+            this.disabled = await false
+            this.dialog = await false
 
         },
-        denyMissionAnswer(){
+        async denyMainMissionAnswer(){
+
+            this.loading = await true
+            this.disabled = await true
+            const uid = await this.nowCard.uid
+            const docId = await this.nowCard.docId
+            const firestore = this.$firestore
+            const score = await this.nowCard.nowStage*10
+            const key = await this.nowCard.key
+            const id = await this.nowCard.id
+            //mainMissionAnswerのcollectionにまず情報更新
+            await firestore.doc(`mainMissionAnswer/${docId}`).update({clear: 6})
+            await console.log("done")
+
+            //team側へのclearの更新
+            await firestore.doc(`Team/${uid}`).get()
+            .then(doc => {
+                const renewedData = doc.data().mainMission.map( x => {
+                    if(x.id === id){
+                        return {id:id, clear: 3}
+                    }else{
+                        return x
+                    }
+                    
+                })
+                firestore.doc(`Team/${uid}`).update({mainMission: renewedData})
+            })
+
+
+            this.nowCard.color = await "black"
+            await this.mainMissionAnswers.map(x => {
+                if(x.key === this.nowCard.key){ return this.nowCard }else{ return x }
+            })
+            this.loading = await false
+            this.disabled = await false
+            this.dialog = await false
+
+
+        },
+        acceptSubStoryAnswer(){
+
+        },
+        denySubStoryAnswer(){
 
         },
         sendMessageToAll(){
@@ -128,7 +224,9 @@ export default {
     },
     computed:{
 
-    }
+        }
+
+    
 }
 </script>
 
